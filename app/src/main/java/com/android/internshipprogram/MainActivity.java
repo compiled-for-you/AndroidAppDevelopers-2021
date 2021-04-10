eb0b05bb38daeb10813fa9fc20814e7085134f6e
Vedansh Bhardwaj <vedwaj@gmail.com>
2021-04-10 14:13:26 +0530

    commiting menu resource file and activity main file after adding toolbar !

--- app/src/main/java/com/android/internshipprogram/MainActivity.java
+++ app/src/main/java/com/android/internshipprogram/MainActivity.java
@@ -1,14 +1,21 @@
 package com.android.internshipprogram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        /**onCreateOptionsMenu(){
+            getMenuInflater().inflate(R.id.menu, menu);
+        }**/
     }
 }
