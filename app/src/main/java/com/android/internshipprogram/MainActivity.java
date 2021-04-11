ede00b93b7adfe4672f2dbe1b678d417cb839ef7
Vedansh Bhardwaj <vedwaj@gmail.com>
2021-04-11 17:49:22 +0530

    Commiting all changes finally completed the app

--- app/src/main/java/com/android/internshipprogram/MainActivity.java
+++ app/src/main/java/com/android/internshipprogram/MainActivity.java
@@ -1,21 +1,39 @@
 package com.android.internshipprogram;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar ;
+    BottomNavigationView btm;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         toolbar = findViewById(R.id.toolbar);
-        /**onCreateOptionsMenu(){
-            getMenuInflater().inflate(R.id.menu, menu);
-        }**/
+        btm = findViewById(R.id.btm);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+        toolbar.setTitle(null);
+        btm.setElevation((float) 10.0);
+        btm.setSelectedItemId(R.id.account);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
     }
 }
