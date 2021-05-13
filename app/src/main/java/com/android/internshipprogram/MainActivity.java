7ff9970ac01a92e9dd23501a55e0030b6f92a5f2
Vedansh Bhardwaj <vedwaj@gmail.com>
2021-05-13 20:18:15 +0530

    Working App

--- app/src/main/java/com/android/internshipprogram/MainActivity.java
+++ app/src/main/java/com/android/internshipprogram/MainActivity.java
@@ -1,20 +1,21 @@
 package com.android.internshipprogram;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.ActionBar;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
-    Toolbar toolbar ;
-    BottomNavigationView btm;
+    public Toolbar toolbar ;
+    public BottomNavigationView btm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +27,44 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(null);
         toolbar.setTitle(null);
-        btm.setElevation((float) 10.0);
         btm.setSelectedItemId(R.id.account);
+
+        btm.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId())
+                {
+                    case R.id.home :
+                        Toast.makeText(MainActivity.this, "Clicked Home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.browse :
+                        Toast.makeText(MainActivity.this, "Clicked Browse", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.account :
+                        Toast.makeText(MainActivity.this, "Already here", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_toolbar, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.notifications)
+        {
+            Toast.makeText(MainActivity.this, "Clicked Notifications", Toast.LENGTH_SHORT).show();
+        }
+        else {
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
